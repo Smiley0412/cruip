4e1b2f37528699fa5f4a1f8f48057590eb927266
smiley <ericsmith.star@gmail.com>
2022-11-30 14:48:20 -0500

    modify sections

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
-import ButtonGroup from '../elements/ButtonGroup';
-import Button from '../elements/Button';
-import Image from '../elements/Image';
-import Modal from '../elements/Modal';
+import React, { useState } from "react";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import ButtonGroup from "../elements/ButtonGroup";
+import Button from "../elements/Button";
+import Image from "../elements/Image";
+import Modal from "../elements/Modal";
 
 const propTypes = {
-  ...SectionProps.types
-}
+  ...SectionProps.types,
+};
 
 const defaultProps = {
-  ...SectionProps.defaults
-}
+  ...SectionProps.defaults,
+};
 
 const Hero = ({
   className,
@@ -24,48 +24,50 @@ const Hero = ({
   invertColor,
   ...props
 }) => {
-
   const [videoModalActive, setVideomodalactive] = useState(false);
 
   const openModal = (e) => {
     e.preventDefault();
     setVideomodalactive(true);
-  }
+  };
 
   const closeModal = (e) => {
     e.preventDefault();
     setVideomodalactive(false);
-  }
+  };
 
   const outerClasses = classNames(
-    'hero section center-content',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
+    "hero section center-content",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
     className
   );
 
   const innerClasses = classNames(
-    'hero-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
+    "hero-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
   );
 
   return (
-    <section
-      {...props}
-      className={outerClasses}
-    >
+    <section {...props} className={outerClasses}>
       <div className="inner-overlay"></div>
       <div className={innerClasses}>
         <div className="hero-content">
           <p className="call">HELLO, WORLD.</p>
-          <h1 className="mt-0 mb-16 reveal-from-bottom name" data-reveal-delay="200">
+          <h1
+            className="mt-0 mb-16 reveal-from-bottom name"
+            data-reveal-delay="200"
+          >
             I'm Eric Smith
           </h1>
           <div className="container-xs">
-            <p className="m-0 mb-32 reveal-from-bottom role" data-reveal-delay="400">
+            <p
+              className="m-0 mb-32 reveal-from-bottom role"
+              data-reveal-delay="400"
+            >
               Solidity & Rust Engineer
             </p>
           </div>
@@ -73,7 +75,7 @@ const Hero = ({
       </div>
     </section>
   );
-}
+};
 
 Hero.propTypes = propTypes;
 Hero.defaultProps = defaultProps;
